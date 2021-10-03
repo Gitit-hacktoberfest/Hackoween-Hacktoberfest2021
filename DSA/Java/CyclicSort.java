7975a7a62dfda122a69cda1b25c3a568be65b490
Kishore <crystalkishore7@gmail.com>
2021-10-03 19:02:10 +0530

    Cyclic Sort Java Program with Explanation and time complexity  is added in Java Folder Successfully

--- DSA/Java/CyclicSort.java
+++ DSA/Java/CyclicSort.java
@@ -6,12 +6,16 @@ where N is the total elements of the array , Then we can perform cyclic sort.
 For example arr = { 5,4,1,2,3 } Since arr is unsorted array and starts from numbers 1 to 5 the
 we can perform Cyclic sort to sort the array.
 
-Time Complexities :
+Time Complexity :
 Here N = No.of.elements present in the array.
 Best case : If the array is already sorted , then we will have N number of comparisons.
 there fore Best case is O(N).
 Worst Case : It will be O(N) again. Lets see worst case example arr={3,5,2,1,4} here we wil
 make 4 swaps + 5 swaps which is (n-1)+(n) = 2n-1 it will be O(N).*/  
+
+Space Complexity :
+Since we dont have any extra variable or array time complexity will be constant.
+therefore O(1)
 
 
 // Code Starts
