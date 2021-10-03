56f2099278919655b14fa1bdd1489732852d68df
debajyoti22 <91139741+debajyoti22@users.noreply.github.com>
2021-10-03 13:11:39 +0530

    Update BubbleSort_inLinkedList.c

--- DSA/cpp/Data Structures/BubbleSort_inLinkedList.c
+++ DSA/cpp/Data Structures/BubbleSort_inLinkedList.c
@@ -7,6 +7,7 @@ struct node{
     };
     struct node*head;
 
+/*declaring the classes for bubble sorting and swapping*/
 void bubblesort();
 void swap(struct node *a, struct node *b);
 
@@ -16,6 +17,7 @@ int main(){
     printf("\n\nEnter no of nodes : ");
     scanf("%d",&n);
     printf("Enter the values : ");
+/* entering the data values for the individual nodes*/
     for(int i = 0;i<n;i++){
         current = (struct node*)malloc(sizeof(struct node));
         scanf("%d",&current->data);
@@ -28,16 +30,18 @@ int main(){
         prev=current;
     }    
 
-    bubblesort();
-
+    bubblesort(); /*calling the bubblesort function*/
     struct node *temp1 = head;
     printf("The sorted values are : ");
+	/*printing the data values*/
     while(temp1 !=NULL){
         printf("%d ",temp1->data);        
         temp1 = temp1->next;
     }
     printf("\n\n");
 }
+
+/*the bubblesort function*/
 void bubblesort(){
     struct node *s = head;
     int swapped, i;
@@ -52,7 +56,7 @@ void bubblesort(){
 		while (ptr1->next != lptr){
 			if (ptr1->data > ptr1->next->data)
 			{
-				swap(ptr1, ptr1->next);
+				swap(ptr1, ptr1->next);/*calling the swap function*/
 				swapped = 1;
 			}
 			ptr1 = ptr1->next;
@@ -61,9 +65,13 @@ void bubblesort(){
 	}
 	
 }
+/*swap function for swapping the variables*/
 void swap(struct node *a, struct node *b)
 {
 	int temp = a->data;
 	a->data = b->data;
 	b->data = temp;
 }
+
+/*Time complexity = O(n^2)
+  Space Complexity = 1 */
