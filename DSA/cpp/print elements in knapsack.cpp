6daa0c11ffba5e47fc1d1828e438b3cd81a82fc5
RISHABH SIDANA <65805267+dinagon@users.noreply.github.com>
2021-10-05 20:15:03 +0530

    Update print elements in knapsack.cpp

--- DSA/cpp/print elements in knapsack.cpp
+++ DSA/cpp/print elements in knapsack.cpp
@@ -1,3 +1,4 @@
+//Problem Statement----Given weights and values of n items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. In other words, given two integer arrays, val[0..n-1] and wt[0..n-1] represent values and weights associated with n items respectively. Also given an integer W which represents knapsack capacity, find out the items such that sum of the weights of those items of a given subset is smaller than or equal to W. You cannot break an item, either pick the complete item or don’t pick it (0-1 property).
 #include <iostream>
 using namespace std;
 int dp[1000][1000];
