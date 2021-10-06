295f329803a62c290884fdd718fcdf097ae87ef0
arvindberi <91769609+arvindberi@users.noreply.github.com>
2021-10-06 19:55:35 +0530

    Create tail_recursion.c

---
+++ DSA/cpp/tail_recursion.c
@@ -0,0 +1,19 @@
+//time complexity: o(n)
+
+#include <stdio.h>
+void fun(int n)
+{
+ if(n>0)
+ {
+   printf("%d ",n);
+   fun(n-1);
+ }
+}
+
+int main() {
+ int x=3;
+ fun(3);
+ return 0;
+}
+
+// O/P: 3 2 1
