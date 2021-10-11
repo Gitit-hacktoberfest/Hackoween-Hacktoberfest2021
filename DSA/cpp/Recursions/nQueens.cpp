1866a06e71227a4889fbabf59ec6a993abe36464
khushijain3103 <khushijain312003@gmail.com>
2021-10-11 12:21:14 +0530

    added problem statement as requested by you

--- DSA/cpp/Recursions/nQueens.cpp
+++ DSA/cpp/Recursions/nQueens.cpp
@@ -1,3 +1,12 @@
+// question statement:
+// The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+// Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+// Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+
+
 #include<vector>
 #include<iostream>
 using namespace std;
