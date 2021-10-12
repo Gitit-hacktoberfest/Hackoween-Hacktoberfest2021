45eab27d74ff105a2f1041d573afacbb056569a6
ashpreetkaur25 <82947364+ashpreetkaur25@users.noreply.github.com>
2021-10-12 09:37:23 +0530

    HeightOfBinaryTree.cpp

---
+++ DSA/cpp/Data Structures/HeightOfBinaryTree.cpp
@@ -0,0 +1,71 @@
+#include<cmath>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+template <typename T>
+
+
+
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+int height(BinaryTreeNode<int> *root){
+	
+	if(root == NULL){
+		return 0;
+	}
+	
+	int Ldepth = height(root -> left);
+	int Rdepth = height(root -> right);
+	return max(Ldepth, Rdepth) + 1;
+	
+}
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    cout << height(root);
+}
