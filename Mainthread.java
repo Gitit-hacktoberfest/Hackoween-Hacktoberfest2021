bdff2c7e35b69bebfa766a0919b0f78abf8db3a7
Vaishnavi Saindane <71937373+vaishnavisaindane@users.noreply.github.com>
2021-10-17 14:16:52 +0530

    Update Mainthread.java

--- Mainthread.java
+++ Mainthread.java
@@ -1,10 +1,15 @@
-public class Mainthread extends Thread {
-  public static void main(String[] args) {
-    Mainthread thread = new Mainthread();
-    thread.start();
+public class Mainthread extends Thread {           //here thread class is provided to create and perform operations on thread.Mainthread extends thread class.
+  
+  public static void main(String[] args) {         //public-access modifier,static method,return type, main-startup of program,command line arguments.
+    
+    Mainthread thread = new Mainthread();          //thread is created or its lifecycle started
+    
+    thread.start();                                //start()method of thread class is used to start a newly created thread.
+    
     System.out.println("This code is outside of the thread");
   }
-  public void run() {
+  public void run() {                              //this fuction is used to perform action for a thread
+    
     System.out.println("This code is running in a thread");
   }
 }
